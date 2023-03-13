072e98d5d3741ae7aeb690efa9d30a85e3fb758c
Philip Keller <philipjoelkeller@gmail.com>
2023-03-13 16:31:13 -0700

    Initial Commit

---
+++ PhotoMania/PhotoManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManiaApp.swift
+//  PhotoMania
+//
+//  Created by Philip Keller on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
