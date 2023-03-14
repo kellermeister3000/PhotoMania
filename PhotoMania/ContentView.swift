0d151bf238f800068ee734c129f67bed25346221
Philip Keller <philipjoelkeller@gmail.com>
2023-03-13 20:49:56 -0700

    First SwiftUI App - iOS Academy https://youtu.be/yyhsTP9NYME

--- PhotoMania/ContentView.swift
+++ PhotoMania/ContentView.swift
@@ -7,13 +7,68 @@
 
 import SwiftUI
 
+class ViewModel: ObservableObject {
+    @Published var image: Image?
+    
+    func fetchNewImage() {
+        guard let url = URL(string: "https://random.imagecdn.app/500/500") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            data, _, _ in
+            guard let data = data else {return}
+            
+            DispatchQueue.main.async {
+                guard let uiImage = UIImage(data: data) else {
+                    return
+                }
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                if let image = viewModel.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.blue)
+                        .padding()
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.blue)
+                        .padding()
+                }
+                
+                Spacer()
+                Spacer()
+                
+                Button {
+                    viewModel.fetchNewImage()
+                } label: {
+                    Text("New Image!")
+                        .bold()
+                    
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                
+            }
+            .navigationBarTitle("Photo Mania")
         }
         .padding()
     }
